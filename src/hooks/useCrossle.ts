8df7889e0b55cce10965cf3cdd7db39052374950
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-16 23:25:00 +0200

    feat: :sparkles: update formatGuess function to include yellow color for misplaced letters and improve keyup handling logic

--- src/hooks/useCrossle.ts
+++ src/hooks/useCrossle.ts
@@ -8,11 +8,51 @@ const useCrossle = (solution: string) => {
     const [history, setHistory] = useState<string[]>([]);
     const [isCorrect, setIsCorrect] = useState(false);
 
-    const formatGuess = (guess: string) => {};
+    const formatGuess = () => {
+        console.log("formatting guess");
+        console.log("current guess: ", currentGuess);
+        const solutionArray: (string | null)[] = [...solution];
+        const formattedGuess = [...currentGuess].map((letter) => {
+            return { key: letter, color: "grey" };
+        });
+
+        formattedGuess.forEach((letter, i) => {
+            if (solutionArray[i] === letter.key) {
+                formattedGuess[i].color = "green";
+                solutionArray[i] = null;
+            }
+        });
+        formattedGuess.forEach((letter, i) => {
+            if (
+                solutionArray.includes(letter.key) &&
+                letter.color !== "green"
+            ) {
+                formattedGuess[i].color = "yellow";
+                solutionArray[solutionArray.indexOf(letter.key)] = null;
+            }
+        });
+        return formattedGuess;
+    };
 
     const addNewGuess = (guess: string) => {};
 
     const handleKeyup = ({ key }: { key: string }) => {
+        if (key === ENTER) {
+            if (turn > 5) {
+                console.log("you used all your guesses");
+                return;
+            }
+            if (history.includes(currentGuess)) {
+                console.log("you already tried that word");
+                return;
+            }
+            if (currentGuess.length !== 5) {
+                console.log("guess must be 5 letters long");
+                return;
+            }
+            const formattedGuess = formatGuess();
+            console.log(formattedGuess);
+        }
         if (key === BACKSPACE) {
             setCurrentGuess((prev) => prev.slice(0, -1));
             return;
