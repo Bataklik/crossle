260725583af9eaa4e423d0f7d67834cdabed769b
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-16 23:02:07 +0200

    refactor: :fire: remove GameManager component and its dependencies

--- src/services/GameManager.ts
+++
@@ -1,7 +0,0 @@
-import { useCrossle } from "../hooks/useCrossle";
-
-export const GameManager = () => {
-    const alleWoorden = useCrossle().alleWoorden;
-    const geheimeWoorden = useCrossle().geheimeWoorden;
-    let geheimewoord = useCrossle().getGeheimWoord;
-};
