17dbebba041cadf7f8364f94e39ddce8bd9b1b6d
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-18 22:40:49 +0200

    feat: :sparkles: add optional style prop to RowCell component

--- src/components/RowCell.tsx
+++ src/components/RowCell.tsx
@@ -1,12 +1,14 @@
 interface RowCellProps {
     guess: string;
     color: string;
+    style?: React.CSSProperties;
 }
 
-export function RowCell({ guess, color }: RowCellProps) {
+export function RowCell({ guess, color, style }: RowCellProps) {
     return (
         <div
-            className={`w-16 h-16 border border-gray-300 flex items-center justify-center text-lg font-bold ${color} `}
+            className={`w-16 h-16 border border-gray-300 flex items-center justify-center text-lg font-bold ${color}`}
+            style={style}
         >
             {guess}
         </div>
